f5581f9cbc03d6cb6002e88b18decfc9c02917ae
Chandan Ojha <chandanojha776@gmail.com>
2023-11-27 21:36:29 +0600

    `updated`

--- laracasts-php-for-beginners-series/fundamentals/functions_and_filters.php
+++ laracasts-php-for-beginners-series/fundamentals/functions_and_filters.php
@@ -7,6 +7,40 @@
   <title>Functions and Filters</title>
 </head>
 
-<body></body>
+<body>
+  <?php
+  $books = [
+    [
+      'name' => 'Do Androids Dream of Electric Sheep',
+      'author' => 'Philip K. Dick',
+      'releaseYear' => 1968,
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'Project Hail Mary',
+      'author' => 'Andy Weir',
+      'releaseYear' => 2021,
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'The Martian',
+      'author' => 'Andy Weir',
+      'releaseYear' => 2011,
+      'purchaseUrl' => 'http://example.com'
+    ],
+  ];
+
+  ?>
+  <ol>
+    <h4>Book List</h4>
+    <?php foreach ($books as $book) : ?>
+      <li>
+        <a href="<?= $book['purchaseUrl'] ?>">
+          <?= $book['name']; ?>(<?= $book['releaseYear'] ?>)
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ol>
+</body>
 
 </html>
