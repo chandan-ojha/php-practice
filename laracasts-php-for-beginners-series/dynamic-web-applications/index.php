bbb05e9c42a5df65f98e40e4590b5f702a6b64d6
Chandan Ojha <chandanojha776@gmail.com>
2023-11-29 19:51:16 +0600

    `pdo-first-steps`

--- laracasts-php-for-beginners-series/dynamic-web-applications/index.php
+++ laracasts-php-for-beginners-series/dynamic-web-applications/index.php
@@ -1,4 +1,25 @@
 <?php
 
 require('functions.php');
-require('router.php');
+// require('router.php');
+
+//connect to MySQL Database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * FROM posts where id = 1");
+
+$statement->execute();
+
+//all post
+// $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
+
+//single post
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['title'] . "</li>";
