78977c5443641655d9ad5648f13640b4ab48ff55
Chandan Ojha <chandanojha776@gmail.com>
2023-11-29 21:44:47 +0600

    `extract-php-database-class`

---
+++ laracasts-php-for-beginners-series/dynamic-web-applications/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+//connect to MySQL Database
+class Database
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($query)
+    {
+
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute();
+
+        return $statement;
+    }
+}
--- laracasts-php-for-beginners-series/dynamic-web-applications/index.php
+++ laracasts-php-for-beginners-series/dynamic-web-applications/index.php
@@ -2,24 +2,9 @@
 
 require('functions.php');
 // require('router.php');
+require('Database.php');
 
-//connect to MySQL Database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+$db = new Database();
+$posts = $db->query("select * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * FROM posts where id = 1");
-
-$statement->execute();
-
-//all post
-// $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// foreach ($posts as $post) {
-//     echo "<li>" . $post['title'] . "</li>";
-// }
-
-//single post
-$post = $statement->fetch(PDO::FETCH_ASSOC);
-
-echo "<li>" . $post['title'] . "</li>";
+dd($posts);
