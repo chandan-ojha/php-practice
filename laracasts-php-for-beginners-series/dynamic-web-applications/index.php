89e3deef1796fa298fcca68b56e42d6ca2f07fb3
Chandan Ojha <chandanojha776@gmail.com>
2023-11-30 11:50:02 +0600

    `sql-injection-vulnerabilities`

--- laracasts-php-for-beginners-series/dynamic-web-applications/index.php
+++ laracasts-php-for-beginners-series/dynamic-web-applications/index.php
@@ -7,7 +7,13 @@ require('Database.php');
 $config = require('config.php');
 $db = new Database($config['database']);
 
+$id = $_GET['id'];
+
+$query = "select * FROM posts where id = :id";
+
+// dd($query);
+
 // $posts = $db->query("select * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
-$posts = $db->query("select * FROM posts")->fetchAll();
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
