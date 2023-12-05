fc8f8f589369a4e37e95004b02b81ae0692398d3
Chandan Ojha <chandanojha776@gmail.com>
2023-12-05 21:46:58 +0600

    `handle-multiple-request-methods-from-a-controller-action`

--- laracasts-php-for-beginners-series/project-organization/controllers/notes/show.php
+++ laracasts-php-for-beginners-series/project-organization/controllers/notes/show.php
@@ -5,16 +5,31 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$currentUserId = 1;
+$currentUserId = 25;
 
-$note = $db->query('SELECT * FROM notes where  id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('SELECT * FROM notes where  id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query('SELECT * FROM notes where  id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
--- laracasts-php-for-beginners-series/project-organization/views/notes/show.view.php
+++ laracasts-php-for-beginners-series/project-organization/views/notes/show.view.php
@@ -8,6 +8,10 @@
             <a href="/notes" class="text-blue-500 hover:underline">go back....</a>
         </p>
         <p><?= htmlspecialchars($note['body']) ?></p>
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
