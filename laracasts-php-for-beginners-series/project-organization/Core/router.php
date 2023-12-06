201fff9c163590cdd120f5c250b0e419658c6d36
Chandan Ojha <chandanojha776@gmail.com>
2023-12-06 11:17:26 +0600

    `build-a-better-router`

--- laracasts-php-for-beginners-series/project-organization/Core/router.php
+++ laracasts-php-for-beginners-series/project-organization/Core/router.php
@@ -1,6 +1,69 @@
 <?php
 
-function routeToController($uri, $routes)
+namespace Core;
+
+class Router
+{
+    protected $routes = [];
+
+    public function add($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method
+        ];
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->add('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->add('POST', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->add('DELETE', $uri, $controller);
+    }
+
+    public function patch($uri, $controller)
+    {
+        $this->add('PATCH', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        $this->add('PUT', $uri, $controller);
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                return require base_path($route['controller']);
+            }
+        }
+
+        $this->abort();
+    }
+
+    protected function abort($code = 404)
+    {
+        http_response_code($code);
+
+        require base_path("views/{$code}.php");
+
+        die();
+    }
+}
+
+//previous approach
+
+/*function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
         require base_path($routes[$uri]);
@@ -21,4 +84,4 @@ function abort($code = 404)
 $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-routeToController($uri, $routes);
+routeToController($uri, $routes);*/
--- laracasts-php-for-beginners-series/project-organization/controllers/notes/show.php
+++ laracasts-php-for-beginners-series/project-organization/controllers/notes/show.php
@@ -5,7 +5,7 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$currentUserId = 25;
+$currentUserId = 1;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $note = $db->query('SELECT * FROM notes where  id = :id', [
