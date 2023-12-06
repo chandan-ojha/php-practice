477898f77a199e18116091222ca9f1dc8f181581
Chandan Ojha <chandanojha776@gmail.com>
2023-12-06 12:03:20 +0600

    `one-request-one-controller`

--- laracasts-php-for-beginners-series/project-organization/controllers/notes/destroy.php
+++ laracasts-php-for-beginners-series/project-organization/controllers/notes/destroy.php
@@ -9,13 +9,14 @@ $currentUserId = 1;
 
 
 $note = $db->query('SELECT * FROM notes where  id = :id', [
-    'id' => $_GET['id']
+    'id' => $_POST['id']
 ])->findOrFail();
-
 
 authorize($note['user_id'] === $currentUserId);
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
+$db->query('delete from notes where id = :id', [
+    'id' => $_POST['id']
 ]);
+
+header('location: /notes');
+exit();
---
+++ laracasts-php-for-beginners-series/project-organization/controllers/notes/store.php
@@ -0,0 +1,29 @@
+<?php
+
+use Core\Database;
+use Core\Validator;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$errors = [];
+
+if (!Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required.';
+}
+
+if (!empty($errors)) {
+    return view("notes/create.view.php", [
+        'heading' => 'Create Note',
+        'errors' => $errors
+    ]);
+}
+
+
+$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => 1
+]);
+
+header('location: /notes');
+die();
