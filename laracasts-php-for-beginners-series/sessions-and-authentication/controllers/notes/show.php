42d19f0885f04b9ec5851ab9b9a5f74db073b94f
Chandan Ojha <chandanojha776@gmail.com>
2023-12-10 10:09:11 +0600

    `sessions-and-authentication-practice-start`

---
+++ laracasts-php-for-beginners-series/sessions-and-authentication/controllers/notes/show.php
@@ -0,0 +1,21 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+
+
+$note = $db->query('SELECT * FROM notes where  id = :id', [
+    'id' => $_GET['id']
+])->findOrFail();
+
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
---
+++ laracasts-php-for-beginners-series/sessions-and-authentication/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*return [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/note' => 'controllers/notes/show.php',
+    '/notes/create' => 'controllers/notes/create.php',
+    '/contact' => 'controllers/contact.php',
+];*/
+
+$router->get('/', 'controllers/index.php');
+$router->get('/about', 'controllers/about.php');
+$router->get('/contact', 'controllers/contact.php');
+
+$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/note', 'controllers/notes/show.php');
+
+$router->get('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes', 'controllers/notes/store.php');
+
+$router->get('/note/edit', 'controllers/notes/edit.php');
+$router->patch('/note', 'controllers/notes/update.php');
+
+$router->delete('/note', 'controllers/notes/destroy.php');
