97fb2e0c65c33612ee4dfe2fa32004821ca24093
Chandan Ojha <chandanojha776@gmail.com>
2023-12-11 16:36:18 +0600

    `middleware-implementation-done`

--- laracasts-php-for-beginners-series/sessions-and-authentication/routes.php
+++ laracasts-php-for-beginners-series/sessions-and-authentication/routes.php
@@ -13,7 +13,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 
 $router->get('/notes/create', 'controllers/notes/create.php');
@@ -24,5 +24,5 @@ $router->patch('/note', 'controllers/notes/update.php');
 
 $router->delete('/note', 'controllers/notes/destroy.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
