c1c77985128391dab3573310caf9cebaa2c7a013
Chandan Ojha <chandanojha776@gmail.com>
2023-12-11 16:57:34 +0600

    `password-bcrypt`

--- laracasts-php-for-beginners-series/sessions-and-authentication/controllers/registration/store.php
+++ laracasts-php-for-beginners-series/sessions-and-authentication/controllers/registration/store.php
@@ -41,7 +41,7 @@ if ($user) {
     //if not, save one to the database, and then log the user in, and redirect.
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     //mark that the user has logged in.
--- laracasts-php-for-beginners-series/sessions-and-authentication/views/index.view.php
+++ laracasts-php-for-beginners-series/sessions-and-authentication/views/index.view.php
@@ -4,7 +4,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello,Welcome to home page!</p>
+        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?>. Welcome to home page!</p>
     </div>
 </main>
 
