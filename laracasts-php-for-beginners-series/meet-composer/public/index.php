1db03a29dc42b0f3016e2c3d2ea7b4a703cb973b
Chandan Ojha <chandanojha776@gmail.com>
2023-12-15 21:05:00 +0600

    `explore-pestphp-package`

--- laracasts-php-for-beginners-series/meet-composer/public/index.php
+++ laracasts-php-for-beginners-series/meet-composer/public/index.php
@@ -9,11 +9,11 @@ session_start();
 
 require BASE_PATH . 'vendor/autoload.php';
 require BASE_PATH . 'Core/functions.php';
-require base_path('bootstrap.php');
+require BASE_PATH . 'bootstrap.php';
 
 
 $router = new \Core\Router();
-$routes = require base_path('routes.php');
+require BASE_PATH . 'routes.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
---
+++ laracasts-php-for-beginners-series/meet-composer/tests/Unit/ContainerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Core\Container;
+
+test('it can resolve something out of the container', function () {
+    //arrange
+    $container = new Container();
+
+    $container->bind('foo', fn () => 'bar');
+
+    //act
+    $result = $container->resolve('foo');
+
+    //assert/expect
+    expect($result)->toEqual('bar');
+});
