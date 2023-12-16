8d5b3c912b6ec76152e6dc2039cc92c21f5bf78a
Chandan Ojha <chandanojha776@gmail.com>
2023-12-16 19:10:08 +0600

    `testing-approaches-practice-done`

---
+++ laracasts/php-for-beginners/testing/tests/Feature/ReferralTest.php
@@ -0,0 +1,9 @@
+<?php
+
+it('allows subscribers to earn money by referring their friends', function () {
+    // Referral
+    // redeem
+});
+
+it('disallows guests from participating in the referral program', function () {
+});
