deedc314108c10f9afba2aa597eaf65e90073a3b
Chandan Ojha <chandanojha776@gmail.com>
2023-12-16 20:05:22 +0600

    `updated`

--- php8/autoloading/Test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Test1
-{
-    public function __construct()
-    {
-        echo "Hello from Test_1";
-    }
-}
--- php8/autoloading/Test2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Test2
-{
-    public function __construct()
-    {
-        echo "Hello from Test_2";
-    }
-}
