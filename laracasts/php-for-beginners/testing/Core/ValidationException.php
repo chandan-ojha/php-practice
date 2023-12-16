56baba00136f150dc8628ac6e5d0701d7e0c0ff2
Chandan Ojha <chandanojha776@gmail.com>
2023-12-16 18:20:58 +0600

    `testing-approaches-practice-start`

---
+++ laracasts/php-for-beginners/testing/Core/ValidationException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core;
+
+use Exception;
+
+class ValidationException extends Exception
+{
+    public readonly array $errors;
+    public readonly array $old;
+
+    public static function throw($errors, $old)
+    {
+        $instance = new static;
+
+        $instance->errors = $errors;
+        $instance->old = $old;
+
+        throw $instance;
+    }
+}
