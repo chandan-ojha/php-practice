d6befe500065f36300b8836e71c7ec871d64e60a
Chandan Ojha <chandanojha776@gmail.com>
2023-12-19 12:48:01 +0600

    `interface-practice-done`

---
+++ laracasts/oop-in-php/interfaces/index.php
@@ -0,0 +1,37 @@
+<?php
+
+interface Newsletter
+{
+    public function subscribe($email);
+}
+
+class CampaignMonitor implements Newsletter
+{
+    public function subscribe($email)
+    {
+        die('subscribing with Campaign Monitor');
+    }
+}
+
+class Drip implements Newsletter
+{
+    public function subscribe($email)
+    {
+        die('subscribing with Drip');
+    }
+}
+
+
+class NewsletterSubscriptionsController
+{
+    public function store(Newsletter $newsletter)
+    {
+        $email = 'chandan@gmail.com';
+
+        $newsletter->subscribe($email);
+    }
+}
+
+$controller = new NewsletterSubscriptionsController();
+
+$controller->store(new CampaignMonitor());
