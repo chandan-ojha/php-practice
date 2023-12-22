fb6e610c16e3b67d765c31e49c51c0a08acfb5e7
Chandan Ojha <chandanojha776@gmail.com>
2023-12-22 12:45:52 +0600

    `better-code-with-oop-by-forcing`

---
+++ ostad-php-fundamentals/php-oop/write-better-code-with-oop-by-forcing/index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Shape
+{
+}
+
+class Shapes
+{
+    private $shapes;
+
+    function __construct()
+    {
+        $this->shapes = array();
+    }
+
+    function addShape(Shape $shape)
+    {
+        array_push($this->shapes, $shape);
+    }
+
+    function totalShapes()
+    {
+        return count($this->shapes);
+    }
+}
+
+class Rectangle extends Shape
+{
+}
+
+class Triangle extends Shape
+{
+}
+
+class Student
+{
+}
+
+$shapesCollection = new Shapes();
+
+$shapesCollection->addShape(new Rectangle());
+$shapesCollection->addShape(new Triangle());
+
+echo $shapesCollection->totalShapes();
