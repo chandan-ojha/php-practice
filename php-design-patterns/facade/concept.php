eb8411d522da2a2a30d02b2a3543bd76c521a94f
Chandan Ojha <chandanojha776@gmail.com>
2023-12-28 11:50:45 +0600

    `facade-pattern`

--- php-design-patterns/facade/concept.php
+++ php-design-patterns/facade/concept.php
@@ -1,1 +1,33 @@
 <?php
+
+//Facade Pattern: Provides a simplified interface to a complex system of classes, library or framework.
+class Sample
+{
+    public function greetings()
+    {
+        echo "Hello Facade!" . PHP_EOL;
+    }
+
+    public function bye()
+    {
+        echo "Bye Facade!" . PHP_EOL;
+    }
+
+    public function sum($a, $b)
+    {
+        echo "Sum: " . ($a + $b) . PHP_EOL;
+    }
+}
+
+//Proxy Pattern: Provides a placeholder for another object to control access to it.
+class SampleFacade
+{
+    public static function __callStatic($name, $arguments)
+    {
+        (new Sample())->{$name}(...$arguments);
+    }
+}
+
+SampleFacade::greetings();
+SampleFacade::bye();
+SampleFacade::sum(75, 25);
