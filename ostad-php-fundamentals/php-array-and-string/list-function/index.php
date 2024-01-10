aacb52a935ea431f9d18e6d1c7a6973d19b1c707
Chandan Ojha <chandanojha776@gmail.com>
2024-01-10 13:41:28 +0600

    #list-function

---
+++ ostad-php-fundamentals/php-array-and-string/list-function/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$colors = array( 122, 233, 344, 455, 566 );
+
+// $red = $colors[0];
+// $green = $colors[1];
+// $blue = $colors[2];
+
+list( $red, $green, $blue ) = $colors;
+
+echo $red . PHP_EOL;
+echo $green . PHP_EOL;
+echo $blue . PHP_EOL;
