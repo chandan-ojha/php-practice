fb00a4ee70dd86792825a826f30ee28f3e7188cb
Chandan Ojha <chandanojha776@gmail.com>
2024-01-12 17:30:30 +0600

    #html-line-break

---
+++ ostad-php-fundamentals/php-array-and-string/html-line-break/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$string = "Lorem ipsum dolor sit amet consectetur, adipisicing elit.\n Voluptate ut veniam, dolorem doloribus nostrum provident, impedit quasi rerum amet aspernatur ullam, corrupti facere dolorum. Voluptatum voluptatem animi rem vero molestias!
+Rem incidunt optio iste soluta.\n Accusamus, voluptatibus rerum officiis porro quas aspernatur laudantium magni facere obcaecati nostrum reiciendis ratione laboriosam soluta, corrupti id eos blanditiis est! Soluta deleniti possimus debitis.\n
+Soluta necessitatibus similique quia voluptates debitis assumenda inventore molestiae, iste excepturi? Quas tempora dolorem eos inventore a, fuga amet porro dolor nesciunt.\n Earum eos molestiae magnam veniam aliquam accusantium repudiandae?
+Deleniti a quibusdam modi accusantium praesentium, veniam animi et eaque aut fugit sunt fuga voluptatibus nisi harum reiciendis possimus eos numquam natus inventore! Illum a doloremque veniam, harum totam similique!
+Sequi necessitatibus iusto iure exercitationem eos fuga neque.\n In tenetur tempora maiores aliquam odit assumenda ipsam. Quisquam illo ipsum corporis. Dicta eaque sit ex aliquid assumenda quae ad, nobis libero.";
+
+// echo $string;
+echo nl2br( $string );
